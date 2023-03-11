7f89220062014852e79165f68328833c4fc9c31d
qiuchengxuan <qiuchengxuan@gmail.com>
2023-03-11 16:32:07 +0800

    Add README

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,4 @@
-//! # sdmmc
-//!
-//! > A sdmmc implementation mainly focusing on embedded system with `no_std` and `async` support
-//!
-//! ## Using this crate
-//!
-//! Assuming you already have `SPI` struct which implements `sdmmc::spi::Transfer`
-//!
-//! ```rust
-//! let mut bus = sdmmc::bus::linux::spi(&args.spi, args.cs).map_err(|e| format!("{:?}", e))?;
-//! let card = bus.init(Delay).await.map_err(|e| format!("{:?}", e))?;
-//! debug!("Card: {:?}", card);
-//! let mut sd = SD::init(spi, card).await.map_err(|e| format!("{:?}", e))?;
-//! let size = Size::from_bytes(sd.num_blocks() as u64 * sd.block_size() as u64);
-//! debug!("Size {}", size);
-//!
-//! let options = SpidevOptions { max_speed_hz: Some(2_000_000), ..Default::default() };
-//! sd.bus(|bus| bus.spi(|spi| spi.0.configure(&options))).unwrap();
-//!
-//! let mut buffer = [0u8; 512];
-//! sd.read(0, slice::from_mut(&mut buffer).iter_mut()).await.map_err(|e| format!("{:?}", e))?;
-//! let mbr = MasterBootRecord::from_bytes(&buffer).map_err(|e| format!("{:?}", e))?;
-//! for partition in mbr.partition_table_entries().iter() {
-//!     println!("{:?}", partition);
-//! }
-//! Ok(())
-//! ```
-
+#![doc = include_str!("../README.md")]
 #![cfg_attr(not(any(test, feature = "std")), no_std)]
 
 extern crate alloc;
